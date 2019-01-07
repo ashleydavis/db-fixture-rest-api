4b3578cf4ce86aa5634b0f6556f92ed3321f33bd
Ashley Davis <ashley@codecapers.com.au>
2019-01-07 16:13:55 +1000

    A REST API to load and unload database fixtures.

--- README.md
+++ README.md
@@ -1,2 +1,68 @@
-# db-fixuture-rest-api
+# db-fixture-rest-api
+
 A REST API for loading and unloading MongoDB database fixtures.
+
+**Don't run this on a production server, it is for testing only!**
+
+Use this in your automated test process to load and unload your database for testing.
+
+This builds upon [node-mongodb-fixtures](https://www.npmjs.com/package/node-mongodb-fixtures).
+
+## Pre-requisites
+
+You must have Node.js installed to run this.
+
+You need a MongoDB instance ready for testing.
+
+## Important files
+
+    fixtures/                   -> Database fixtures live under this directory.
+        example-json-fixture/   -> An example database fixture in JSON format.
+        example-js-fixture/     -> An example database fixture in JavaScript format.
+
+## REST API end points
+
+This REST API exposes the following endpoints:
+
+- HTTP GET http://localhost:3555/load-fixture?name=your-fixture-name
+- HTTP GET http://localhost:3555/unload-fixture?name=your-fixture-name
+
+The default port number is 3555, but you can set this to any value as described in the section *Use it in your project* below.
+
+## Trial run
+
+Clone or download this repo. Open a command line and change directory to the repo then install dependencies:
+
+    cd db-fixture-rest-api
+    npm install
+
+Now run the REST API:
+
+    npm start
+
+Now use curl or your fav REST API client (eg [ARC](https://install.advancedrestclient.com) or [Postman](https://www.getpostman.com/)), hit the HTTP endpoints documented in the previous section to load and unload your database fixtures.
+
+Two example database fixtures are included under the *fixtures* sub-directory.
+
+You can load them using the names *example-js-fixture* and *example-json-fixture*.
+
+For example to load *example-js-fixture* hit http://localhost:3555/load-fixture?name=example-json-fixture with HTTP GET request.
+
+After you have loaded a fixture browse your database to check that the data has been loaded correctly. By default it is loaded under a database named *my-test-database*.
+
+After that you can unload the fixture and then check that the data was removed from the database.
+
+For example to unload *example-js-fixture* hit http://localhost:3555/unload-fixture?name=example-json-fixture with HTTP GET request.
+
+## Use it in your project
+
+To use this REST API to help test your own project please customize it by setting the following environment variables:
+
+- FIXTURES_DIR - Set the location of the fixtures directory (defaults to 'fixtures').
+- PORT - Set the port number of the REST API (defaults to 3555).
+- DBHOST - Set the host address for the MongoDB server instance (defaults to mongodb://localhost:27017).
+- DBNAME - Name of the databse to load and unload fixtures to (defaults to my-test-database).
+
+## Going further
+
+Please see [node-mongodb-fixtures](https://www.npmjs.com/package/node-mongodb-fixtures) for more of what you can do with these database fixtures.
---
+++ index.js
@@ -0,0 +1,124 @@
+"use strict";
+
+const inProduction = process.env.NODE_ENV === "production";
+if (inProduction) {
+    throw new Error("Don't run DB FIXTURE API in production!!");
+}
+
+const express = require('express');
+const Fixtures = require('node-mongodb-fixtures');
+const path = require('path');
+
+const app = express();
+
+const fixturesDirectory = process.env.FIXTURES_DIR || "fixtures";
+const port = process.env.PORT || 3555;
+const databaseHost = process.env.DBHOST || "mongodb://localhost:27017";
+const dbName = process.env.DBNAME || "my-test-database";
+console.log("Using DBHOST " + databaseHost);
+console.log("Using DB " + dbName);
+
+//
+// Start the HTTP server.
+//
+function startServer() {
+    return new Promise((resolve, reject) => {
+        var server = app.listen(port, err => {
+            if (err) {
+                reject(err);
+                return;
+            }
+
+            const addrInfo = server.address();
+            const host = addrInfo.address;
+            const port = addrInfo.port;
+            console.log("DB fixture REST API listening at http://%s:%s", host, port);
+            console.log("Please please your database fixtures in the 'fixtures' sub-directory.")
+            console.log("Use the following endpoints to load and unload your database fixtures:");
+            console.log(`HTTP GET http://localhost:${port}/load-fixture?name=your-fixture-name`);
+            console.log(`HTTP GET http://localhost:${port}/unload-fixture?name=your-fixture-name`);
+
+            resolve(server);
+        });
+    });
+}
+
+//
+// Load a fixture to the database.
+//
+async function loadFixture (fixtureName) {
+    const fixtures = new Fixtures({
+        dir: path.join(fixturesDirectory, fixtureName),
+        mute: false,
+    });
+
+    await fixtures.connect(databaseHost + "/" + dbName);
+    await fixtures.unload();
+    await fixtures.load();
+    await fixtures.disconnect();
+}
+
+//
+// Unload a fixture from the database.
+//
+async function unloadFixture(fixtureName) {
+    const fixtures = new Fixtures({
+        dir: path.join(fixturesDirectory, fixtureName),
+        mute: false,
+    });
+
+    await fixtures.connect(databaseHost + "/" + dbName);
+    await fixtures.unload();
+    await fixtures.disconnect();
+}
+
+async function main() {
+
+    app.get("/load-fixture", (req, res) => {
+        if (!req.query && !req.query.name) {
+            res.status(400).send("Specify query parameter 'name'");
+        }
+
+        const fixtureName = req.query.name;
+        loadFixture(fixtureName)
+            .then(() => {
+                console.log("Loaded database fixture: " + fixtureName);
+                res.sendStatus(200);
+            })
+            .catch(err => {
+                const msg = "Failed to load database fixture " + fixtureName;
+                console.error(msg);
+                console.error(err && err.stack || err);
+                res.status(400).send(msg);
+            });
+    });
+
+    app.get("/unload-fixture", (req, res) => {
+        if (!req.query && !req.query.name) {
+            res.status(400).send("Specify query parameter 'name'");
+        }
+
+        const fixtureName = req.query.name;
+        unloadFixture(fixtureName)
+            .then(() => {
+                console.log("Unloaded database fixture: " + fixtureName);
+                res.sendStatus(200);
+            })
+            .catch(err => {
+                const msg = "Failed to unload database fixture " + fixtureName;
+                console.error(msg);
+                console.error(err && err.stack || err);
+                res.status(400).send(msg);
+            });
+    });
+    
+    await startServer();
+}
+
+main()
+    .catch(err => {
+        console.error("DB fixture REST API failed to start.");
+        console.error(err && err.stack || err);
+    });
+
+
